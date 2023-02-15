1de5aa4f2adf33dc6c15adf0afce15c0b2c9924d
cyplenokk <ivanova.polina.ig@gmail.com>
2023-02-15 11:13:24 +0300

    dont know

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 s = []
+print(type(s))
+
+letters = ['A', 'B']
+
+a = list('список')
+print(a)
+
+fruits = ['клубника', 'овощ', 'арбуз', 'картофель']
+print(fruits[3])
+
+print(len(fruits))
+
+fruits.extend([1, 5])
+print(fruits)
